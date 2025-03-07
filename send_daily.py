0ec8b6d2d6ee62a55f481848e8ec2ff717359386
Ahmed Nasr <ahmed@ahmednasr.me>
2025-03-06 23:27:28 -0600

    send message module

---
+++ send_daily.py
@@ -0,0 +1,14 @@
+import telebot
+import os
+import datetime
+from dotenv import load_dotenv, dotenv_values
+from telebot import util
+
+load_dotenv()
+
+def send_message(file_path):
+    bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
+    file = open(file_path,'r',encoding='utf-8')
+    text = file.read()
+    bot.send_message(os.getenv('TGReciptor'),text,parse_mode="Markdown")
+    return True
