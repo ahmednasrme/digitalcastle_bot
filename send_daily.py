768a41858694448994ee0024b36c3af66d926259
Ahmed Nasr <ahmed@ahmednasr.me>
2025-03-08 10:53:32 -0600

    Correct TGReciptor

--- send_daily.py
+++ send_daily.py
@@ -10,5 +10,5 @@ def send_message(file_path):
     bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
     file = open(file_path,'r',encoding='utf-8')
     text = file.read()
-    bot.send_message(os.getenv('TGReciptor'),text,parse_mode="Markdown")
+    bot.send_message(int(os.getenv('TGReciptor')),text,parse_mode="Markdown")
     return True
