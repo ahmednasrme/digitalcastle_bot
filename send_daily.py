c4518257ec54a20231c8f6d89f6d2000f1e349c8
Ahmed Nasr <ahmed@ahmednasr.me>
2025-03-08 10:58:25 -0600

    module working independently;

--- send_daily.py
+++ send_daily.py
@@ -10,5 +10,11 @@ def send_message(file_path):
     bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
     file = open(file_path,'r',encoding='utf-8')
     text = file.read()
-    bot.send_message(int(os.getenv('TGReciptor')),text,parse_mode="Markdown")
+    bot.send_message(os.getenv('TGReciptor'),text,parse_mode="Markdown")
+    file.close()
     return True
+
+if __name__ == "__main__":
+    file_path = f'data/message{datetime.datetime.date(datetime.datetime.now())}.md'
+    send_message(file_path)
+    print('message sent')
